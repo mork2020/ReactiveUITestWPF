85636031823351ec23dac01f8e62fa8d7226b52f
Perun <pierre.schwarz@gmx.de>
2020-06-15 14:38:01 +0200

    Initial commit

---
+++ AppViewModel.cs
@@ -0,0 +1,34 @@
+﻿using ReactiveUI;
+using System;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace ReactiveUITestWPF
+{
+    public class AppViewModel : ReactiveObject
+    {      
+        private string _searchTerm;
+        public string SearchTerm
+        {
+            get => _searchTerm;
+            set => this.RaiseAndSetIfChanged(ref _searchTerm, value);
+        }
+       
+        public AppViewModel()
+        {                                 
+            this
+                .WhenAnyValue(x => x.SearchTerm)
+                .Skip(1) // ignore the initial NullOrEmpty value of SearchTerm                                                        
+                .Subscribe(val =>
+                {
+                    String n = val;
+                    if (n.Equals("aaa"))
+                    {
+                        n = "bbb";
+                    }                  
+                    Console.WriteLine("WhenAnyValue Subscribe() -> " + $"SearchTerm value changed to: \"{n}\"\n");
+                    SearchTerm = n;
+                });            
+        }     
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using ReactiveUI;
+using System.Reactive.Disposables;
+
+namespace ReactiveUITestWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : ReactiveWindow<AppViewModel>
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ViewModel = new AppViewModel();
+           
+            this.WhenActivated(disposableRegistration =>
+            {               
+                this.Bind(ViewModel,
+                    viewModel => viewModel.SearchTerm,
+                    view => view.searchTextBox.Text)
+                    .DisposeWith(disposableRegistration);
+            });
+        }
+    }
+}
